2ec29a3e9c2885651c266a7a6f2022ed4a5af2fa
Vitor Nere <vitor.nere@hotmail.com>
2022-06-03 23:03:24 -0300

    Exporting

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,6 @@
 export { default as EnpyreDisplay } from './components/EnpyreDisplay';
 export { default as EnpyreProvider } from './contexts';
+export { default as EnpyreEditor } from './components/EnpyreEditor';
 export { useApp } from './hooks/App';
+export { useCode } from './hooks/Code';
+export { usePyodide } from './hooks/Pyodide';
