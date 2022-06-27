1bc3fa557c60972d4d087b89701ba517bc65b1b7
Vitor Nere <vitor.nere@hotmail.com>
2022-06-26 21:27:43 -0300

    Releasing new alpha stable version

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
 const constants = {
   enpyrePyURL:
     process.env.NODE_ENV === 'production'
-      ? 'enpyre==0.0.5'
-      : 'http://localhost:8080/enpyre-0.0.5-py3-none-any.whl',
+      ? 'enpyre==0.0.6'
+      : 'http://localhost:8080/enpyre-0.0.6-py3-none-any.whl',
 };
 
 export default constants;
